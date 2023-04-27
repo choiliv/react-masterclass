b5a8d502576307140481b54bee359b222f541dc9
choiliv <104757153+choiliv@users.noreply.github.com>
2023-04-27 21:12:39 +0900

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import styled from "styled-components";
+import React from "react";
+import Circle from "./Circle";
+
+
+
+function App() {
+  return <div>
+    <Circle bgColor="teal" borderColor="tomato"/>
+    <Circle bgColor="tomato"/>
+  </div>;
+}
+
+export default App;
